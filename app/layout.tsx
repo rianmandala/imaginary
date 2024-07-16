71e5a5c3c1e1710870b325b2b71ccacd8d86fd4d
rian <rianmandala959@gmail.com>
2024-07-17 06:07:27 +0700

    feat: update metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,10 +12,45 @@ const IBMPlex = IBM_Plex_Sans({
 });
 
 export const metadata: Metadata = {
-  title: "Imaginary",
-  description: "AI-powered image generator",
+  title:
+    "Imaginary - Advanced Image Editing Services | Background Removal, Image Restoration & More",
+  description:
+    "Imaginary offers advanced image editing services including background removal, image restoration, image recoloring, object removal, and generative fill. Perfect your photos with our AI-powered tools. Try Imaginary today!",
   icons: {
     icon: "/assets/images/logo-icon.svg",
+  },
+  keywords:
+    "image editing, background removal, image restoration, image recoloring, object removal, generative fill, photo editing, photo restoration, image enhancement",
+  openGraph: {
+    title: "Imaginary - Advanced Image Editing Services",
+    description:
+      "Transform your photos with Imaginary's AI-powered tools for background removal, image restoration, recoloring, and more.",
+    url: "https://imaginary-od9j.vercel.app/",
+    type: "website",
+    images: [
+      {
+        url: "https://res.cloudinary.com/dgtcoctbo/image/upload/v1721170989/trfciocd8h34brsr6k4p.png",
+        width: 800,
+        height: 600,
+        alt: "Imaginary Logo",
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Imaginary - Advanced Image Editing Services",
+    description:
+      "Transform your photos with Imaginary's AI-powered tools for background removal, image restoration, recoloring, and more.",
+    images: [
+      {
+        url: "https://res.cloudinary.com/dgtcoctbo/image/upload/v1721170989/trfciocd8h34brsr6k4p.png",
+        alt: "Imaginary Logo",
+      },
+    ],
+  },
+  robots: {
+    index: true,
+    follow: true,
   },
 };
 
